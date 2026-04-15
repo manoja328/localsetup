f9f1d50384bef3e1be8c03d73dd0bf692aec6504
Manoj Acharya <manoja328@gmail.com>
2026-04-15 10:06:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+## for file explorer in wezterm
+brew install yazi
 ## for tunnels 
 brew install boring
